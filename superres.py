b0505017b0bcbda288cc17150acaabf81ff71567
Alexankharin <42934906+Alexankharin@users.noreply.github.com>
2020-07-04 16:46:39 +0300

    Add files via upload
    Added median-reduction trained weights (better for most of games)

--- superres.py
+++ superres.py
@@ -56,10 +56,14 @@ out=tf.keras.layers.Lambda(denormalize)(x)
 rtsrn=tf.keras.models.Model(inputs=inp, outputs=out)
 
 rtsrn.load_weights('RTSR_NEAREST.h5')
+rtsrm=tf.keras.models.clone_model(rtsrn)
+rtsrm.load_weights('RTSR_MEDIAN.h5')
 inp=rtsrn.input
 out=tf.keras.backend.clip(rtsrn.output, 0, 255)
 myclippededsrn=tf.keras.models.Model(inputs=inp, outputs=out)
-
+inp=rtsrm.input
+out=tf.keras.backend.clip(rtsrm.output, 0, 255)
+myclippededsrm=tf.keras.models.Model(inputs=inp, outputs=out)
 for i in range(7,0,-1):
     time.sleep(1)
     print ('make Game Window active for capturing, {} s. remains'.format(i))
@@ -74,6 +78,8 @@ windowsize=[0,0]
 FPS=10.0
 enhance=False
 sct=mss()
+resizemode=0
+displaydims=(GetSystemMetrics(0),GetSystemMetrics(1))
 while True:
     starttime=time.time()
     if platform.system()=='Windows':  
@@ -87,13 +93,16 @@ while True:
     if enhance==0:
         img3=np.array(Image.fromarray(imageinit).resize((imageinit.shape[1]*4//pixels[1],imageinit.shape[0]*4//pixels[0]),Image.NEAREST))
     if enhance==1:
-        img3=np.array(Image.fromarray(imageinit).resize((imageinit.shape[1]*4//pixels[1],imageinit.shape[0]*4//pixels[0]),Image.NEAREST))
-        #img2 = np.array(Image.fromarray(imageinit).resize((imageinit.shape[1]//pixels[1],imageinit.shape[0]//pixels[0]),Image.NEAREST))
-        #img3=myclippededsr.predict(np.expand_dims(img2, 0))[0]
+        img2 = np.array(Image.fromarray(imageinit).resize((imageinit.shape[1]//pixels[1],imageinit.shape[0]//pixels[0]),Image.NEAREST))
+        img3=myclippededsrm.predict(np.expand_dims(img2, 0))[0]
     if enhance==2:
         img2 = np.array(Image.fromarray(imageinit).resize((imageinit.shape[1]//pixels[1],imageinit.shape[0]//pixels[0]),Image.NEAREST))
         img3=myclippededsrn.predict(np.expand_dims(img2, 0))[0]
-    img5=img3.astype(np.uint8)
+    img4=img3.astype(np.uint8)
+    #if resizemode==1:
+    #    img5 = np.array(Image.fromarray(img4).resize((displaydims[0]-50,displaydims[1]-50)))
+    if resizemode==0:
+        img5=img4
     cv2.putText(img5,'FPS {:.2f} SR {}'.format(FPS, enhance),(30,30),cv2.FONT_HERSHEY_PLAIN, 1,(0,0,255),thickness=1)
     cv2.imshow('RTSR', img5)
     k = cv2.waitKey(1) 
@@ -105,6 +114,8 @@ while True:
     if k == ord('2'):
         pixels[1]=2
         pixels[0]=2
+    if k == ord('r'):
+        resizemode=1-resizemode    
     if k == ord('w'):
         shiftzero[1]=shiftzero[1]+1
     if k == ord('s'):
